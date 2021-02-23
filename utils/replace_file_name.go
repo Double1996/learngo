54fcd5d2c9bef43e000c1033d9f7c06d30caccdb
double2 <shuanglei@xiaoyouzi.com>
2021-02-23 18:16:42 +0800

    给女朋友替换文件名称脚本

--- utils/replace_file_name.go
+++ utils/replace_file_name.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"os"
 	"path/filepath"
 	"strings"
@@ -10,32 +11,49 @@ import (
 /**
 批量修改文件夹内 文件名称
 */
+var rMap = map[string]string{
+	"格律法学院：": "",
+	"-------格律法学院（微信订阅号：gelvfaxueyuan）": "",
+	"-格律法学院（微信公众号gelvfaxueyuan)":        "",
+	"------格律法学院（微信订阅号：gelvfaxueyuan）":  "",
+}
+
+var dirs = []string{
+	"C:\\Users\\Administrator\\Downloads\\1741个合同范本",
+	"C:\\Users\\Administrator\\Downloads\\格律法学院：1080个各省公布的合同范本\\格律法学院：1080个各省公布的合同范本",
+}
+
 func main() {
 	// 解析命令行参数
 	var dir string
 	flag.StringVar(&dir, "d", "", "文件夹名称")
-	var pattern string
-	flag.StringVar(&pattern, "p", "", "name pattern, eg. newname%d")
 
 	flag.Parse()
-	if dir == "" || pattern == "" {
-		flag.Usage()
-		return
-	}
 
 	// 遍历文件夹，获取文件路径
 	paths := make([]string, 0)
-	filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
-		if !info.IsDir() {
-			paths = append(paths, path)
-		}
-		return nil
-	})
-
-	// 遍历文件路径，修改文件名
-	for _, path := range paths {
-		newPath := strings.Replace(path, pattern, "", -1)
-		//newPath := filepath.Join(filepath.Dir(path), fmt.Sprintf(pattern, i+1)+filepath.Ext(path))
-		os.Rename(path, newPath)
+	for _, dir := range dirs {
+		filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+			if !info.IsDir() {
+				paths = append(paths, path)
+			}
+			return nil
+		})
 	}
+	// 遍历文件路径，修改文件名
+	i := 0
+	for _, path := range paths {
+		for k, v := range rMap {
+			newPath := strings.Replace(path, k, v, -1)
+			err := os.Rename(path, newPath)
+			if err != nil {
+				fmt.Println(err.Error())
+			}
+			fmt.Println(i, newPath)
+		}
+		i += 1
+		fmt.Println(i, path)
+
+	}
+	fmt.Printf("-------------sucess-----------------------")
 }
